02575a8b635a08f6372fd028cf9c87c038853d20
sedson <seamus.edson@gmail.com>
2024-09-09 13:04:33 -0400

    use rgb(from ...) syntax for color fades
    update build script for cleanliness
    
    add posts to scraps

--- scripts/build.js
+++ scripts/build.js
@@ -8,6 +8,7 @@ const flattenContent = require('./flattenContent');
 const compileTemplate = require('./compileTemplate');
 const widgets = require('./createWidgets');
 
+
 /**
  * The cached template. So I don't load or compile them so many times.
  */
@@ -15,11 +16,25 @@ const _templates = {};
 
 
 /**
+ * The date of this build.
+ */
+const date = getDateFormat();
+
+
+/**
+ * 
+ */
+const statics = {
+  footer: template('footer')({ date }),
+};
+
+
+/**
  * Read a content file and flatten the content to html.
  * @param {string} file The path to read.
  * @returns {object} A document that can be passed to a template function.
  */
-const get = (path) => {
+function get(path) {
   const file = fs.readFileSync(path, 'utf8');
   const doc = parseContent(file);
   doc.content = flattenContent(doc.content);
@@ -32,7 +47,7 @@ const get = (path) => {
  * @param {string} file The path to write.
  * @param {string} content The stuff to write.
  */
-const write = (file, content) => {
+function write(file, content) {
   const dir = path.dirname(file);
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true });
@@ -47,7 +62,7 @@ const write = (file, content) => {
  * @param {string} file 
  * @returns 
  */
-const template = (file) => {
+function template(file) {
   if (!_templates[file]) {
     const template = fs.readFileSync(`templates/${file}.template.html`, 'utf8');
     _templates[file] = compileTemplate(template);
@@ -56,24 +71,38 @@ const template = (file) => {
 }
 
 
-
 /**
  * Get a date, nicely formatted.
  * @param {string} file
  * @returns {String}
  */
-const getDateFormat = (selectedDate) => {
+function getDateFormat(selectedDate) {
   let date = selectedDate ?? new Date();
   return date.toISOString().split('T')[0];
 }
 
 
 /**
- * Get the navigation bread crumb for a particular path.
+ * Get a numeric sort value from a string like 'spring_2024' or 
+ * 'winter_2022-2023'. In this case for sorting the sketchbook folders 
+ * chronologically.
+ * @param {string} str The season_year string
+ * @returns {number}
  */
-const getNav = (link) => {
+function seasonYearToSortValue(str) {
+  const seasons = ['spring', 'summer', 'fall', 'winter'];
+  const [season, year] = str.split('_');
+  return Number.parseInt(year) + seasons.indexOf(season) / 4;
+}
+
+
+/**
+ * Get the navigation bread crumb for a particular path.
+ * @param {string} link
+ */
+function getNav(link) {
   if (!link) return;
-  let out = ''
+  let out = '';
   let href = '/';
   const parts = link.split('/');
   for (let i = 0; i < parts.length; i++) {
@@ -91,230 +120,125 @@ const getNav = (link) => {
 
 
 /**
- * Build the site.
- * @returns 
+ * Build the header
  */
-function main() {
+function header(ctx) {
+  return template('header')({ ...ctx, ...widgets });
+}
 
-  console.time('main');
-  const date = getDateFormat();
 
-  const t_page = ctx => {
-    return template('main')({ ...ctx, date });
-  }
-  const t_header = (ctx) => {
-    return template('header')({ ...ctx, ...widgets });
-  }
-  const t_footer = template('footer');
-  const t_homePage = template('homepage');
+function page(ctx) {
+  return template('main')({ date, ...statics, ...ctx });
+}
 
-  // Header and footer are statics.
-  const footer = t_footer({ date });
-  const contact = template('contact')
 
-  // Loop over all the projects in source and make pages.
-  let projects = [];
-  let selectedProjects = [];
+function fetchProjects() {
   let projectList = fs.readdirSync('source/projects');
-  let selectedIndex = 1;
+  let projects = [];
+  let selected = [];
 
   for (let file of projectList) {
-
-    let slug = file.split('.')[0];
     let project = get('source/projects/' + file);
+    project.slug = file.split('.')[0];
 
-    // Projects with publish = true get their own page.
-    if (project.publish === 'true' && !project.link) {
-      project.link = '/projects/' + slug;
-    }
+    // Cast string to bool.
+    project.publish = (project.publish === 'true');
 
-    projects.push(project);
-    project.tag = 'a';
-
-    project.sortValue = Number.parseInt(project.year) ?? 2017;
-    project.sortValue += Number(project.month ?? 12) / 12;
-    if (project.publish === 'false') {
-      project.sortValue -= 2000;
-    }
-
-    // Just div if not set to publish.
-    if (project.publish !== 'true') {
+    // Projects with publish true get their own page.
+    if (project.publish) {
+      project.link = '/projects/' + project.slug;
+      project.tag = 'a';
+    } else {
       project.classList = 'inactive';
       project.tag = 'div';
     }
 
-    write('www/projects/' + slug + '/index.html', t_page({
-      header: t_header({ nav: getNav('projects/' + slug) }),
-      footer,
-      date,
-      title: project.title,
-      body: template('projectpage')(project)
-    }));
+    projects.push(project);
 
     if (project.selected === 'true') {
-      selectedIndex++;
-      selectedProjects.push(project);
+      selected.push(project);
     }
   }
 
-  const projectSort = (a, b) => {
-    return b.sortValue - a.sortValue;
+  function sortOperation(a, b) {
+    const sA = Number(a.year) + Number(a.month ?? 13) / 12 + (a.publish ? 1000 : 0);
+    const sB = Number(b.year) + Number(b.month ?? 13) / 12 + (b.publish ? 1000 : 0);
+    return sB - sA;
   }
 
   // Sort the projects by year.
-  projects = projects
-    .sort(projectSort)
-    .map(a => template('projectrow')(a))
-    .join('\n');
+  projects.sort(sortOperation);
+  selected.sort(sortOperation);
+  return { projects, selected };
+}
 
-  selectedProjects = selectedProjects
-    .sort(projectSort)
-    .map((a, i) => {
-      return template('projectcard')({ ...a, index: i + 1 });
-    })
-    .join('\n');
 
-  // ----------------------------------
-  // HOME
-  // ----------------------------------
-  write('www/index.html', t_page({
-    header: t_header(),
-    footer,
+// –-- HOME ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+/**
+ * Build the home page.
+ */
+function home() {
+  write('www/index.html', page({
+    header: header(),
     title: 'Seamus Edson',
     body: template('homepage')({
       content: get('source/home.txt').content,
-      projects: selectedProjects,
     }),
   }));
+}
 
-  // ----------------------------------
-  // PROJECTS
-  // ----------------------------------
-  write('www/projects/index.html', t_page({
-    header: t_header({ nav: getNav('projects') }),
-    footer,
+
+// –-- PROJECTS ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+/**
+ * Build the one off project pages
+ */
+function projectPage(project) {
+  write('www/projects/' + project.slug + '/index.html', page({
+    header: header({ nav: getNav('projects/' + project.slug) }),
+    title: project.title,
+    body: template('projectpage')(project)
+  }));
+}
+
+/**
+ * Build the project index.
+ */
+function projectIndex({ projects, selected }) {
+  write('www/projects/index.html', page({
+    header: header({ nav: getNav('projects') }),
     title: 'Seamus Edson',
-    body: template('projects')({
-      selected: selectedProjects,
-      projects,
-    })
+    body: template('projects')({ selected, projects }),
   }));
+}
 
 
-  // ----------------------------------
-  // The plain text default pages
-  // ----------------------------------
-  const plainPages = [
-    ['About', get('source/about.txt').content],
-  ];
-
-
-
-  for (let p of plainPages) {
-    const pageLc = p[0].toLowerCase();
-    write(`www/${pageLc}/index.html`, t_page({
-      header: t_header({ nav: getNav(pageLc) }),
-      footer,
-      title: `Seamus Edson - ${p[0]}`,
-      body: template('page')({
-        content: p[1]
-      }),
-    }));
-  }
-
-  // ----------------------------------
-  // Sketchbook
-  // ----------------------------------
-  const folders = fs.readdirSync('www/img/sketchbook')
-    .filter(x => x[0] !== '.'); // Filter out .DS_Store
-
-  const seasons = ['spring', 'summer', 'fall', 'winter'];
-
-  folders.sort((a, b) => {
-    let [aSeason, aYear] = a.split('_');
-    let [bSeason, bYear] = b.split('_');
-    aYear = aYear.split('-')[0];
-    bYear = bYear.split('-')[0];
-    const yearSort = bYear - aYear;
-    const seasonSort = (seasons.indexOf(bSeason) - seasons.indexOf(aSeason)) / 10;
-    return yearSort + seasonSort;
-  });
-
-  const groups = folders.map(dir => {
-    const files = fs.readdirSync(`www/img/sketchbook/${dir}`)
-      .filter((x, i) => x[0] !== '.');
-
-    const images = files.map(file => {
-      return template('sketchbook-image')({ file, url: `/img/sketchbook/${dir}/${file}` });
-    });
-
-    const links = files.map((_, i) => {
-      return `<li>${i}</li>`;
-    }).join('\n');
-
-    return template('sketchbook-collection')({
-      images: images.join('\n'),
-      count: images.length,
-      links: links,
-      folder: dir,
-      img: `/img/sketchbook/${dir}/${files[0]}`,
-      arrow: widgets.arrow,
-    });
-  });
-
-
-  write('www/sketchbook/index.html', t_page({
-    header: t_header({ nav: getNav('sketchbook') }),
-    footer,
-    title: 'Seamus Edson - Sketchbook',
-    body: template('sketchbook')({
-      content: groups.join('\n'),
+// –-- ABOUT  ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––----
+/**
+ * Build the abotu page.
+ */
+function about() {
+  write(`www/about/index.html`, page({
+    header: header({ nav: getNav('about') }),
+    title: `Seamus Edson - About`,
+    body: template('page')({
+      content: get('source/about.txt').content,
     }),
   }));
+}
 
 
-
-  // Loop over all the posts in scraps and make pages.
-  let posts = [];
-  let postsJson = {};
-  let postList = fs.readdirSync('source/scraps');
-
-  for (let fileName of postList) {
-    let post = get('source/scraps/' + fileName);
-
-    if (post.publish === 'false') {
-      continue;
-    }
-
-    post.date = getDateFormat(new Date(post.timestamp));
-    postsJson[post.id] = post;
-    posts.push(template('post')(post));
-  }
-
-  write('www/scraps/posts.json', JSON.stringify(postsJson, null, 2));
-
-  // ----------------------------------
-  // FEED
-  // ----------------------------------
-  write('www/scraps/index.html', t_page({
-    header: t_header({ nav: getNav('scraps') }),
-    footer,
-    title: 'Seamus Edson - Feed',
-    body: template('scraps')({ posts: posts.join('\n'), ...widgets }),
-  }));
-
-  // ----------------------------------
-  // COLOPHON
-  // ----------------------------------
+// –-- COLOPHON  ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––-
+/**
+ * Build the colophon page.
+ */
+function colophon() {
   let log = fs.readFileSync('log.txt', 'utf8');
   log = log.split('\n').slice(0, 3).join('\n');
   log = parseContent(log);
   log = flattenContent(log.content);
 
-
-  write('www/colophon/index.html', t_page({
-    header: t_header({ nav: getNav('colophon') }),
-    footer,
+  write('www/colophon/index.html', page({
+    header: header({ nav: getNav('colophon') }),
     title: 'Seamus Edson - Colophon',
     body: template('colophon')({
       content: get('source/colophon.txt').content,
@@ -322,7 +246,112 @@ function main() {
     })
   }));
 
+}
+
+// –-- SCRAPS  –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+/**
+ * Build the scraps page.
+ */
+function scraps() {
+  let postList = fs.readdirSync('source/scraps');
+  let posts = [];
+  let postsJson = {};
+
+  for (let fileName of postList) {
+    let post = get('source/scraps/' + fileName);
+    if (post.publish === 'false') {
+      continue;
+    }
+    post.date = getDateFormat(new Date(post.timestamp));
+    postsJson[post.id] = post;
+    posts.push(post);
+  }
+
+  posts.sort((a, b) => {
+    return new Date(a.timestamp) > new Date(b.timestamp) ? -1 : 1;
+  });
+
+  write('www/scraps/posts.json', JSON.stringify(postsJson, null, 2));
+
+  write('www/scraps/index.html', page({
+    header: header({ nav: getNav('scraps') }),
+    title: 'Seamus Edson - Scraps',
+    body: template('scraps')({
+      posts: posts.map(p => template('post')(p)).join('\n'),
+      ...widgets
+    }),
+  }));
+}
+
+// –-- SKETCH  –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+/**
+ * Build the sketchbook page.
+ */
+function sketchbook() {
+  const dirs = fs.readdirSync('www/img/sketchbook')
+    .filter(x => x[0] !== '.')
+    .sort((a, b) => {
+      return seasonYearToSortValue(b) - seasonYearToSortValue(a);
+    });
+
+  const groups = dirs.map(dir => {
+    const files = fs.readdirSync(`www/img/sketchbook/${dir}`)
+      .filter((x, i) => x[0] !== '.');
+
+
+    const images = files.map(file => {
+      return template('sketchbook-image')({ url: `/img/sketchbook/${dir}/${file}` });
+    }).join('\n');
+
+    const links = files.map((_, i) => `<li>${i}</li>`).join('\n');
+
+    return template('sketchbook-collection')({
+      images,
+      count: files.length,
+      links,
+      folder: dir,
+      img: `/img/sketchbook/${dir}/${files[0]}`,
+      ...widgets,
+    });
+  });
+
+  write('www/sketchbook/index.html', page({
+    header: header({ nav: getNav('sketchbook') }),
+    title: 'Seamus Edson - Sketchbook',
+    body: template('sketchbook')({
+      content: groups.join('\n'),
+    }),
+  }));
+}
+
+
+/**
+ * Build the site.
+ * @returns 
+ */
+function main() {
+  console.time('main');
+
+  home();
+  about();
+  colophon();
+  scraps();
+  sketchbook();
+
+  const { projects, selected } = fetchProjects();
+
+  projects.filter(p => p.publish).forEach(p => {
+    projectPage(p);
+  });
+
+  projectIndex({
+    projects: projects.map(p => template('projectrow')(p)).join('\n'),
+    selected: selected.map((p, i) => template('projectcard')({ ...p, index: i + 1 })).join('\n'),
+  });
+
   console.timeEnd('main');
 }
+
+
 
 main();
