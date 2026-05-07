0c27e725b0becc3a4e774c6098976cf9b9f6511d
Seamus <seamus.edson@gmail.com>
2026-05-06 21:40:45 -0400

    project flex justification, tweak copy

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 rm -rf www/
 cp -Rp assets/ www/
-git log --pretty=format:"%ad %s" --date=short > log.txt
+
+LATEST=$(git log -1 --pretty=format:"%ad %s" --date=short)
+
+if [ -f log.txt ]; then
+  FIRST=$(head -n 1 log.txt)
+
+  if [ "$FIRST" != "$LATEST" ]; then
+    {
+      echo "$LATEST"
+      cat log.txt
+    } > log.tmp && mv log.tmp log.txt
+  fi
+else
+  echo "$LATEST" > log.txt
+fi
+
 node scripts/build.js
